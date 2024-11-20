dcc14863990e866c36ec7fc636b6b71b862c639d
brongulus <prashantrameshtak@gmail.com>
2024-11-20 05:57:31 +0530

    Added controller and base test
    Still need to verify all branches, also have to add multiple
    cases that checks when secrets are unused, when there are
    multiple secrets, when updating fails and so on.

--- internal/controller/pod_controller.go
+++ internal/controller/pod_controller.go
@@ -21,6 +21,8 @@ import (
 
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/sets"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
@@ -36,6 +38,38 @@ type PodReconciler struct {
 // +kubebuilder:rbac:groups=core,resources=pods/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=core,resources=pods/finalizers,verbs=update
 
+// Sets the immutable field for the given secret as true
+func (r *PodReconciler) tagSecretAsImmutable(ctx context.Context, req ctrl.Request, secretName string) (string, error) {
+	log := log.FromContext(ctx)
+	log.V(1).Info(secretName)
+
+	// Get Secret
+	secret := &corev1.Secret{}
+	secretNamespacedName := types.NamespacedName{
+		Name:      secretName,
+		Namespace: req.Namespace,
+	}
+	if err := r.Get(ctx, secretNamespacedName, secret); err != nil {
+		log.Error(err, "Could not fetch secret")
+		return secretName, client.IgnoreNotFound(err)
+	}
+
+	// Tag Immutable if not
+	if secret.Immutable == nil || *secret.Immutable != true {
+		shouldBeImmutable := true
+		secret.Immutable = &shouldBeImmutable
+		if err := r.Update(ctx, secret); err != nil {
+			log.Error(err, "Could not update secret")
+			return secretName, err
+		}
+		log.V(1).Info("Secret is made immutable")
+	} else {
+		log.V(1).Info("Secret is already set")
+	}
+	// Return
+	return secretName, nil
+}
+
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
 // TODO(user): Modify the Reconcile function to compare the state specified by
@@ -46,9 +80,63 @@ type PodReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.19.0/pkg/reconcile
 func (r *PodReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	log := log.FromContext(ctx)
 
-	// TODO(user): your logic here
+	pod := &corev1.Pod{}
+	if err := r.Get(ctx, req.NamespacedName, pod); err != nil {
+		log.Error(err, "Could not fetch pod")
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	// TODO: Check if namespace is not kube-system etc
+	if req.Namespace == "kube-system" || req.Namespace == "local-path-storage" {
+		return ctrl.Result{}, nil
+	}
+
+	log.Info(pod.Name)
+
+	// Get list of all the secrets attached to a pod
+	secretList := sets.New[string]()
+
+	// pod.Volumes.Secret.SecretName
+	for _, volume := range pod.Spec.Volumes {
+		if volume.Secret != nil {
+			secretList.Insert(volume.Secret.SecretName)
+		}
+	}
+
+	// Ref: https://stackoverflow.com/questions/46406596/how-to-identify-unused-secrets-in-kubernetes
+	for _, container := range pod.Spec.Containers {
+		// pod.Containers.Env.ValueFrom.SecretKeyRef.Name
+		for _, env := range container.Env {
+			if env.ValueFrom != nil && env.ValueFrom.SecretKeyRef != nil {
+				secretList.Insert(env.ValueFrom.SecretKeyRef.Name)
+			}
+		}
+		// pod.Containers.EnvFrom.SecretRef.Name
+		for _, envFrom := range container.EnvFrom {
+			if envFrom.SecretRef != nil {
+				secretList.Insert(envFrom.SecretRef.Name)
+			}
+		}
+	}
+	// pod.ImagePullSecrets.Name
+	for _, imagePullSecret := range pod.Spec.ImagePullSecrets {
+		if imagePullSecret.Name != "" { // FIXME
+			secretList.Insert(imagePullSecret.Name)
+		}
+	}
+
+	// Tag each secret as immutable
+	for secret := range secretList {
+		// FIXME: Is it an issue if early return happens i.e.
+		// it errors out while only tagging one secret and the
+		// rest aren't updated? Maybe call reconcile again
+		_, err := r.tagSecretAsImmutable(ctx, req, secret)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+	}
 
 	return ctrl.Result{}, nil
 }
--- internal/controller/pod_controller_test.go
+++ internal/controller/pod_controller_test.go
@@ -17,16 +17,92 @@ limitations under the License.
 package controller
 
 import (
+	"context"
+	"time"
+
 	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 )
 
 var _ = Describe("Pod Controller", func() {
-	Context("When reconciling a resource", func() {
+	const (
+		PodName        = "test-pod"
+		TestNamespace  = "default"
+		TestSecretName = "test-secret"
 
-		It("should successfully reconcile the resource", func() {
+		timeout  = time.Second * 10
+		duration = time.Second * 10
+		interval = time.Millisecond * 250
+	)
 
+	Context("When adding a pod having a secret", func() {
+		It("should make the secret immutable", func() {
 			// TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
 			// Example: If you expect a certain status condition after reconciliation, verify it here.
+
+			// DONE try changing the order of creation of secret and pod (doesnt matter)
+			By("By creating a new Secret")
+			ctx := context.Background()
+			testSecret := &corev1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      TestSecretName,
+					Namespace: TestNamespace,
+				},
+				Type: "Opaque",
+			}
+			Expect(k8sClient.Create(ctx, testSecret)).To(Succeed())
+
+			secretLookupKey := types.NamespacedName{Name: TestSecretName, Namespace: TestNamespace}
+			createdSecret := &corev1.Secret{}
+
+			Eventually(func(g Gomega) {
+				g.Expect(k8sClient.Get(ctx, secretLookupKey, createdSecret)).To(Succeed())
+			}, timeout, interval).Should(Succeed())
+
+			By("By creating a new Pod")
+			testPod := &corev1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      PodName,
+					Namespace: TestNamespace,
+				},
+				Spec: corev1.PodSpec{
+					Volumes: []corev1.Volume{
+						{
+							Name: "credentials",
+							VolumeSource: corev1.VolumeSource{
+								Secret: &corev1.SecretVolumeSource{
+									SecretName: TestSecretName,
+								},
+							},
+						},
+					},
+					Containers: []corev1.Container{
+						{
+							Name:  "secret-container",
+							Image: "alpine:latest",
+						},
+					},
+				},
+			}
+			Expect(k8sClient.Create(ctx, testPod)).To(Succeed())
+
+			podLookupKey := types.NamespacedName{Name: PodName, Namespace: TestNamespace}
+			createdPod := &corev1.Pod{}
+
+			Eventually(func(g Gomega) {
+				g.Expect(k8sClient.Get(ctx, podLookupKey, createdPod)).To(Succeed())
+			}, timeout, interval).Should(Succeed())
+
+			By("Checking that the attached secret is immutable")
+			Eventually(func(g Gomega) {
+				g.Expect(k8sClient.Get(ctx, podLookupKey, createdPod)).To(Succeed(), "should GET the Pod")
+				g.Expect(k8sClient.Get(ctx, secretLookupKey, createdSecret)).To(Succeed(), "should GET the Secret")
+				g.Expect(createdPod.Spec.Volumes[0].Secret.SecretName).To(Equal(createdSecret.Name), "secret should be attached")
+				g.Expect(createdSecret.Immutable).To(HaveValue(Equal(true)), "secret should be Immutable") // Equal does strict type check, hance HaveValue
+			}, timeout, interval).Should(Succeed(), "should attach our secret to the pod")
 		})
 	})
 })
